42b4227f7f07b626e8302bb8700558e8a6746f4e
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-02 23:49:53 -0800

    Add files via upload
    This app uses JSON file.
    - Extracts questions and answers from Json file
    - Displays question in windows form interface
    - Creates dynamic buttons for displaying Answers
    - Evaluates users choice and if correct, adds point to total score
    - After the last question displays total score percentage

---
+++ QuizApp/QuizApp.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace QuizInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quizNum = new System.Windows.Forms.Label();
+            this.textQuestion = new System.Windows.Forms.Label();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.infoLbl = new System.Windows.Forms.Label();
+            this.welcomeNote = new System.Windows.Forms.Label();
+            this.scoreLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // quizNum
+            // 
+            this.quizNum.AutoSize = true;
+            this.quizNum.Location = new System.Drawing.Point(22, 10);
+            this.quizNum.Name = "quizNum";
+            this.quizNum.Size = new System.Drawing.Size(96, 25);
+            this.quizNum.TabIndex = 0;
+            this.quizNum.Text = "quizNum";
+            // 
+            // textQuestion
+            // 
+            this.textQuestion.AutoSize = true;
+            this.textQuestion.CausesValidation = false;
+            this.textQuestion.Location = new System.Drawing.Point(60, 10);
+            this.textQuestion.MaximumSize = new System.Drawing.Size(800, 300);
+            this.textQuestion.MinimumSize = new System.Drawing.Size(200, 10);
+            this.textQuestion.Name = "textQuestion";
+            this.textQuestion.Size = new System.Drawing.Size(200, 25);
+            this.textQuestion.TabIndex = 1;
+            this.textQuestion.Text = "question";
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Font = new System.Drawing.Font("Calibri", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextBtn.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.nextBtn.Location = new System.Drawing.Point(669, 650);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Padding = new System.Windows.Forms.Padding(10);
+            this.nextBtn.Size = new System.Drawing.Size(174, 69);
+            this.nextBtn.TabIndex = 2;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // infoLbl
+            // 
+            this.infoLbl.AutoSize = true;
+            this.infoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLbl.Location = new System.Drawing.Point(93, 560);
+            this.infoLbl.Name = "infoLbl";
+            this.infoLbl.Size = new System.Drawing.Size(58, 31);
+            this.infoLbl.TabIndex = 3;
+            this.infoLbl.Text = "info";
+            // 
+            // welcomeNote
+            // 
+            this.welcomeNote.AutoSize = true;
+            this.welcomeNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeNote.Location = new System.Drawing.Point(663, 167);
+            this.welcomeNote.Name = "welcomeNote";
+            this.welcomeNote.Size = new System.Drawing.Size(126, 31);
+            this.welcomeNote.TabIndex = 4;
+            this.welcomeNote.Text = "Welcome";
+            // 
+            // scoreLbl
+            // 
+            this.scoreLbl.AutoSize = true;
+            this.scoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLbl.Location = new System.Drawing.Point(1338, 10);
+            this.scoreLbl.Name = "scoreLbl";
+            this.scoreLbl.Size = new System.Drawing.Size(81, 31);
+            this.scoreLbl.TabIndex = 5;
+            this.scoreLbl.Text = "score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1520, 791);
+            this.Controls.Add(this.scoreLbl);
+            this.Controls.Add(this.welcomeNote);
+            this.Controls.Add(this.infoLbl);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.textQuestion);
+            this.Controls.Add(this.quizNum);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label quizNum;
+        private System.Windows.Forms.Label textQuestion;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Label infoLbl;
+        private System.Windows.Forms.Label welcomeNote;
+        private System.Windows.Forms.Label scoreLbl;
+    }
+}
+
---
+++ QuizApp/QuizApp.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace QuizInterface
+{
+    public partial class Form1 : Form
+    {
+        /* Global variables */
+        Button btn;
+
+        //first question
+        int idx = 0;
+
+        int score = 0;
+        int scoreChoice = 0;
+        int totalScore = 0;
+        bool value = false;
+ 
+        int questionNumber = 1; // used to number each question
+        string result = "ans"; // used to check answer
+
+        public Form1()
+        {
+            InitializeComponent();
+            // StartPosition was set to FormStartPosition.Manual in the properties window.
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+            int w = Width >= screen.Width ? screen.Width : (screen.Width + Width) / 4;
+            int h = Height >= screen.Height ? screen.Height : (screen.Height + Height) / 4;
+
+            this.Location = new Point((screen.Width - w) / 2, (screen.Height - h) / 2);
+            this.Size = new Size(w, h);
+   
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            infoLbl.Visible = false;
+            textQuestion.Visible = false;
+            quizNum.Visible = false;
+            scoreLbl.Visible = false;
+            EntryNote();
+            welcomeNote.Visible = false;
+        }
+
+        public void EntryNote()
+        {
+            welcomeNote.Text = "Welcome to Microsoft System and Software Academy compitency test. \r\nThere are 40 questions.\r\nFor each question you will receive 1 points. \r\nYour score will be added and displayed at the end. \r\n\nGood Luck! \r\nHit next to start your test.";
+            welcomeNote.Location = new Point(120, 100);
+            welcomeNote.Size = new Size(300, 40);
+        }
+
+        
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            // Clear and Reset
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load_1(e, e);
+
+            // Read JSON file
+            string compitencyQuiz = File.ReadAllText(@"Compitency.json");
+            Random rnd = new Random();
+            QuestionCollection quesCollection;
+
+            welcomeNote.Visible = false;
+            scoreLbl.Visible = true;
+            scoreLbl.Text = "Score: " + totalScore.ToString();
+            scoreLbl.Location = new Point(600, 10);
+
+            textQuestion.Visible = true;
+            quizNum.Visible = true;
+
+            quesCollection = JsonConvert.DeserializeObject<QuestionCollection>(compitencyQuiz);
+            // generate random number
+            //List<int> quizNumContainer = new List<int>();
+            //idx = rnd.Next(0, quesCollection.Questions.Count);
+            //while (quizNumContainer.Contains(idx) == true)
+            //{
+            //    idx = rnd.Next(0, quesCollection.Questions.Count);
+            //}
+            //quizNumContainer.Add(idx);
+
+            // check if last question
+           if (idx < quesCollection.Questions.Count)
+            {
+                // display question number
+                quizNum.Text = questionNumber.ToString() + ")";
+                quizNum.Location = new Point(0, 10);
+                quizNum.Padding = new Padding(5, 0, 5, 0);
+                questionNumber++;
+
+                // display question
+                textQuestion.Text = quesCollection.Questions[idx].Quiz.ToString();
+                textQuestion.Location = new Point(30, 10);
+                textQuestion.Padding = new Padding(0, 0, 0, 10);
+
+                // display answer
+                var answer = quesCollection.Questions[idx].Answer;
+
+                // Create Dynamic Answers/Button
+                // place dynamic button
+                int i = 0;
+                int top = textQuestion.Height + 10;
+                foreach (KeyValuePair<string, bool> item in answer)
+                {
+                    btn = new Button();
+                    btn.Name = "ans" + i.ToString();
+                    btn.Text = item.Key;
+                    btn.AutoSize = true;
+                    btn.Location = new Point(60, top);
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 1;
+                    btn.FlatAppearance.BorderColor = Color.LightBlue;
+                    this.Controls.Add(btn);
+                    btn.Click += new EventHandler(this.button_click);
+                    top += btn.Height + 10;
+                    i += 1;
+                    // capture correct answer
+                    if (item.Value)
+                    {
+                        result = btn.Name;
+                    }
+
+                }
+
+                // place label
+                infoLbl.Location = new Point(60, top + 10);
+
+                // Dynamic placement of Button NEXT
+                nextBtn.Location = new Point(this.Width / 2, top + 30);
+
+                // resets choice
+                scoreChoice = 0;
+
+                // next question
+                idx += 1;
+            }
+           else
+            {
+                // if last question
+                afterLastQuestion(totalScore, quesCollection.Questions.Count, e);
+            }
+
+        }
+
+        // Dynamic button even handler
+        void button_click(object sender, EventArgs e)
+        {
+            // get avtive button
+            Button btn = sender as Button;
+
+            // check if active button is correct
+            if (btn.Name == result)
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 1;
+                btn.FlatAppearance.BorderColor = Color.Green;
+            }
+            else if (btn.Name != result)
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 1;
+                btn.FlatAppearance.BorderColor = Color.Red;
+            }
+            // display info Correct/Wrong
+            value = (btn.Name == result);
+            information(value);
+
+            // takes only first answer
+            scoreChoice += 1;
+
+            // get score
+            totalScore = scoreCalc(scoreChoice, value);
+
+            // highlight next button
+            nextBtn.FlatStyle = FlatStyle.Flat;
+            nextBtn.FlatAppearance.BorderSize = 1;
+            nextBtn.FlatAppearance.BorderColor = Color.Blue;
+
+        }
+
+        // score calculator
+        public int scoreCalc(int x, bool y)
+        {
+            
+            if (x <= 1 && y)
+            {
+                score += 1;
+            }
+            return score;
+        }
+
+        // Correct/Wrong info
+        void information(bool condition)
+        {
+            infoLbl.Visible = true;
+            infoLbl.ForeColor = Color.Red;
+
+            if (condition)
+            {
+                infoLbl.ForeColor = Color.Green;
+                infoLbl.Text = "Correct!";
+            }
+            else
+            {
+                infoLbl.Text = "Incorrect!";
+            }  
+        }
+
+        public void afterLastQuestion(int x, int count, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load_1(e, e);
+
+            infoLbl.Visible = false;
+            textQuestion.Visible = false;
+            quizNum.Visible = false;
+            scoreLbl.Visible = false;
+            nextBtn.Visible = false;
+
+            welcomeNote.Visible = true;
+            welcomeNote.Location = new Point(240, 100);
+
+            float flt1 = x;
+            float flt2 = count;
+            float calc = (flt1 / flt2) * 100;
+            int total = Convert.ToInt16(calc);
+
+            if (total >= 60)
+            {
+
+                welcomeNote.Text = "Congratulation! You passed.\r\nYou scored: " + total.ToString() + "%";
+            }
+            else
+            {
+                welcomeNote.Text = "Sorry! You did not pass.\r\nYou scored: " + total.ToString() + "%";
+            }
+        }
+    }
+}
